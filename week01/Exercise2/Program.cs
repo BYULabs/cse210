098a1368e8659ece3b81eadf9d115b42f8765be0
Sebastian Iturralde <itusebastian@gmail.com>
2026-02-28 15:49:12 -0500

    C# Programming Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(gradePercentage);
+
+        string sign = "";
+
+        if (grade % 10 >= 7 && grade < 90)
+        {
+            sign = "+";
+        }
+        else if (grade % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"Your letter grade is an A{sign}.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"Your letter grade is a B{sign}.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"Your letter grade is a C{sign}.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"Your letter grade is a D{sign}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade is an F.");
+        }
+        
     }
 }
