2e9377f982623fade73e871f57a4608f76078bf2
Sebastian Iturralde <itusebastian@gmail.com>
2026-03-20 11:46:16 -0500

    Refactor scripture initialization to include new references and corresponding texts

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,13 +7,29 @@ class Program
     {
         List<Scripture> scriptures = new List<Scripture>();
         
-        var ref1 = new Reference("John", 3, 16);
-        var scripture1 = new Scripture(ref1, "For God so loved the world that he gave his one and only Son");
+        var ref1 = new Reference("Ether", 12, 27);
+        var scripture1 = new Scripture(ref1, "I will make weak things become strong unto them that have faith in me");
         scriptures.Add(scripture1);
         
-        var ref2 = new Reference("Proverbs", 3, 5, 6);
-        var scripture2 = new Scripture(ref2, "Trust in the Lord with all your heart and lean not on your own understanding");
+        var ref2 = new Reference("Mosiah", 3, 19);
+        var scripture2 = new Scripture(ref2, "The natural man is an enemy to God and has been since the fall of Adam");
         scriptures.Add(scripture2);
+        
+        var ref3 = new Reference("1 Nephi", 3, 7);
+        var scripture3 = new Scripture(ref3, "I will go and do the things which the Lord hath commanded for I know that the Lord giveth no commandments unto the children of men save he shall prepare a way for them");
+        scriptures.Add(scripture3);
+        
+        var ref4 = new Reference("Alma", 32, 21);
+        var scripture4 = new Scripture(ref4, "Faith is not to have a perfect knowledge of things therefore if ye have faith ye hope for things which are not seen which are true");
+        scriptures.Add(scripture4);
+        
+        var ref5 = new Reference("Mosiah", 2, 17);
+        var scripture5 = new Scripture(ref5, "When ye are in the service of your fellow beings ye are only in the service of your God");
+        scriptures.Add(scripture5);
+        
+        var ref6 = new Reference("2 Nephi", 2, 25);
+        var scripture6 = new Scripture(ref6, "Adam fell that men might be and men are that they might have joy");
+        scriptures.Add(scripture6);
         
         Random random = new Random();
         int randomIndex = random.Next(scriptures.Count);
