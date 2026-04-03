f325bca69fca221328908bf49c5e029ee8232260
Sebastian Iturralde <itusebastian@gmail.com>
2026-04-03 11:05:24 -0500

    Implement ReflectionActivity with prompts and questions for deeper user reflection

--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -1,17 +1,80 @@
 class ReflectionActivity : Activity
 {
+    private string[] _prompts =
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private string[] _questions =
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    private Random _random = new Random();
+
     public ReflectionActivity()
         : base(
             "Reflection Activity",
-            "This activity will help you reflect on times in your life when you have shown strength and resilience.")
+            "This activity will help you reflect on times in your life when you have shown strength and resilience. " +
+            "This will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
     }
 
     public void Run()
     {
         DisplayStartingMessage();
+
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(1500);
+
+        string selectedPrompt = _prompts[_random.Next(_prompts.Length)];
+
         Console.WriteLine();
-        Console.WriteLine("[Part 1] Reflection activity logic will be added next.");
+        Console.WriteLine("Consider the following prompt:");
+        Console.WriteLine();
+        Console.WriteLine($"--- {selectedPrompt} ---");
+        Console.WriteLine();
+        Console.WriteLine("When you have something in mind, press Enter to continue.");
+        Console.ReadLine();
+
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.WriteLine("You may begin in:");
+
+        for (int countdown = 5; countdown > 0; countdown--)
+        {
+            Console.Write(countdown);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+
+        Console.WriteLine();
+
+        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+        int questionIndex = _random.Next(_questions.Length);
+
+        Console.Clear();
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write($"> {_questions[questionIndex]} ");
+            ShowSpinner(10000);
+            Console.WriteLine();
+
+            questionIndex = (questionIndex + 1) % _questions.Length;
+        }
+
         DisplayEndingMessage();
     }
 }
