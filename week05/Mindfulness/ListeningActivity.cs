5dac215d7f6f4b0f237c3f0130e65902bbbf2426
Sebastian Iturralde <itusebastian@gmail.com>
2026-04-03 13:49:42 -0500

    Implement ListingActivity with prompt selection and item counting logic

--- week05/Mindfulness/ListeningActivity.cs
+++ week05/Mindfulness/ListeningActivity.cs
@@ -1,17 +1,63 @@
 class ListingActivity : Activity
 {
+    private string[] Prompts =
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    private Random _random = new Random();
+
     public ListingActivity()
         : base(
             "Listing Activity",
-            "This activity will help you reflect on the good things in your life by having you list as many things as you can.")
+            "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
     {
     }
 
     public void Run()
     {
         DisplayStartingMessage();
+
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        ShowSpinner(1500);
+
+        string selectedPrompt = Prompts[_random.Next(Prompts.Length)];
+
         Console.WriteLine();
-        Console.WriteLine("[Part 1] Listing activity logic will be added next.");
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine();
+        Console.WriteLine($"--- {selectedPrompt} ---");
+        Console.WriteLine();
+        Console.Write("You may begin in: ");
+
+        for (int countdown = 5; countdown > 0; countdown--)
+        {
+            Console.Write(countdown);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        int itemCount = 0;
+        DateTime endTime = DateTime.Now.AddSeconds(GetDuration());
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            itemCount++;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"You listed {itemCount} items!");
+
         DisplayEndingMessage();
     }
 }
