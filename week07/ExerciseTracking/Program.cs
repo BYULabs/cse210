8cb6c2f73d13e7995d8874f70362794387aaf071
Sebastian Iturralde <itusebastian@gmail.com>
2026-04-13 21:56:21 -0500

    Add SwimmingActivity class to track swimming activities with laps, distance, speed, and pace calculations

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,7 +7,8 @@ class Program
         List<Activity> activities = new List<Activity>
         {
             new RunningActivity(new DateTime(2026, 04, 3), 30, 3.0),
-            new CyclingActivity(new DateTime(2026, 04, 04), 45, 12.0)
+            new CyclingActivity(new DateTime(2026, 04, 04), 45, 12.0),
+            new SwimmingActivity(new DateTime(2026, 04, 05), 40, 30)
         };
 
         foreach (Activity activity in activities)
